b97e5948c769f6253006acea595b15422d76d087
wangxf2000 <wangxf.2000@gmail.com>
2020-08-10 13:41:38 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+kinit -kt /run/cloudera-scm-agent/process/*-hdfs-NAMENODE/hdfs.keytab  hdfs/`hostname`@CLOUDERA.COM   
+hdfs dfs -mkdir /user/admin
+hdfs dfs -chown admin:admin /user/admin
+
+
+kinit -kt /run/cloudera-scm-agent/process/*-hive-HIVESERVER2/hive.keytab  hive/`hostname`@CLOUDERA.COM   
+sh create_hive_data.sh 
+beeline -u "jdbc:hive2://`hostname`:10000/default;principal=hive/_HOST@CLOUDERA.COM;" -f grant.sql
+beeline -u "jdbc:hive2://`hostname`:10000/default;principal=hive/_HOST@CLOUDERA.COM;" -f hive_test.sql
+impala-shell -f impala_create_kudu.sql
