777fad337f1c140f34179fa5b8093821fb0df099
wangxf2000 <wangxf.2000@gmail.com>
2020-08-11 11:57:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ hdfs dfs -chown admin:admin /user/admin
 kinit -kt /run/cloudera-scm-agent/process/*-hive-HIVESERVER2/hive.keytab  hive/`hostname`@CLOUDERA.COM   
 sh create_hive_data.sh 
 beeline -u "jdbc:hive2://`hostname`:10000/default;principal=hive/_HOST@CLOUDERA.COM;" -f grant.sql
-beeline -u "jdbc:hive2://`hostname`:10000/default;principal=hive/_HOST@CLOUDERA.COM;" -f hive_test.sql
+beeline -u "jdbc:hive2://`hostname`:10000/default;principal=hive/_HOST@CLOUDERA.COM;" -f create_hive_test_data.sql
 impala-shell -f impala_create_kudu.sql
