644b9ea1edc2e0585c8f6d476cf75d43c1bba0b2
wangxf2000 <wangxf.2000@gmail.com>
2021-10-24 09:01:03 +0800

    Update run_no_kerberos.sh

--- run_no_kerberos.sh
+++ run_no_kerberos.sh
@@ -4,3 +4,7 @@ hdfs dfs -chmod -R 777  /tmp/data/test/
 hdfs dfs -chmod 777 /user
 beeline -u "jdbc:hive2://`hostname`:10000/default" -f create_hive_test_data.sql
 impala-shell -f impala_create_kudu.sql
+# add etl_user for BDR
+useradd -p `openssl passwd -1 -salt 'cloudera' cloudera` etl_user
+hdfs dfs -mkdir /user/etl_user
+hdfs dfs -chown etl_user:hadoop /user/etl_user
