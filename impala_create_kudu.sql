018f6f9566e7f3d20e3272046eb84a566f5e7817
wangxf2000 <wangxf.2000@gmail.com>
2021-10-24 09:56:51 +0800

    Update impala_create_kudu.sql

--- impala_create_kudu.sql
+++ impala_create_kudu.sql
@@ -1,4 +1,4 @@
-CREATE TABLE test_kudu
+CREATE TABLE testdata.test_kudu
 (
 id INT, name STRING, password STRING, salary INT,
 PRIMARY KEY(id)
@@ -6,6 +6,6 @@ PRIMARY KEY(id)
 PARTITION BY HASH PARTITIONS 16
 STORED AS KUDU
 ;
-insert into test_kudu select * from test;
+insert into testdata.test_kudu select * from testdata.test;
 quit;
 
